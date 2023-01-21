a07a64b8e80604e07209cbc7c3eac4fdc799b810
Moyses Azevedo <m.mirazev@gmail.com>
2023-01-21 14:13:45 -0300

    add identify in model

--- MunchkinsCalculator/Domain/Model/PlayerModel.swift
+++ MunchkinsCalculator/Domain/Model/PlayerModel.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
-struct PlayerModel {
+struct PlayerModel: Identifiable {
+    var id = UUID()
     var name: String
     var level: Int
     var gear: Int
     var sorcery: Int
+
+    init(name: String, level: Int, gear: Int, sorcery: Int) {
+        self.name = name
+        self.level = level
+        self.gear = gear
+        self.sorcery = sorcery
+    }
 }
