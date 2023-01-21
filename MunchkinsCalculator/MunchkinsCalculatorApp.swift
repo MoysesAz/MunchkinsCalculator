e322057f76059f07e2d2c6c627975e99930b04ea
Moyses Azevedo <m.mirazev@gmail.com>
2023-01-21 12:12:49 -0300

    first commit

---
+++ MunchkinsCalculator/MunchkinsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchkinsCalculatorApp.swift
+//  MunchkinsCalculator
+//
+//  Created by Moyses Miranda do Vale Azevedo on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MunchkinsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
