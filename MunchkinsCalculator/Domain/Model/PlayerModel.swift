34b1d642d17d29951436f9901835cb47ff06c75e
Moyses Azevedo <m.mirazev@gmail.com>
2023-01-21 12:54:49 -0300

    add PlayerModel

---
+++ MunchkinsCalculator/Domain/Model/PlayerModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerModel.swift
+//  MunchkinsCalculator
+//
+//  Created by Moyses Miranda do Vale Azevedo on 21/01/23.
+//
+
+import Foundation
+
+struct PlayerModel {
+    var name: String
+    var level: Int
+    var gear: Int
+    var sorcery: Int
+}
